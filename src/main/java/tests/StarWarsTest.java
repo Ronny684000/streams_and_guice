90cb443a1eef55fffdfcba601cddb9032a46b850
Ronny684000 <77720177+Ronny684000@users.noreply.github.com>
2022-02-03 12:22:45 +0300

    Update StarWarsTest.java

--- src/main/java/tests/StarWarsTest.java
+++ src/main/java/tests/StarWarsTest.java
@@ -9,26 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static utils.ApiUtils.*;
 import static utils.RandomUtils.*;
 
-public class StarWarsTest extends BaseTest {
+class StarWarsTest extends BaseTest {
 
     private static final String CHECKING_DATE = "1990-01-01";
 
     @ParameterizedTest
     @MethodSource("filmProvider")
-    void case3(List<String> filmNames) {
+    void case1(List<String> filmNames) {
         assertThat(checkContainsList(restassuredController.getFilms(), filmNames))
                 .overridingErrorMessage("Films are not match").isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void case4(int filmId) {
+    void case2(int filmId) {
         assertThat(checkReleaseDate(restassuredController.getFilm(filmId), CHECKING_DATE))
                 .overridingErrorMessage("The film was shot later").isTrue();
     }
 
     @Test
-    void case5() {
+    void case3() {
         assertThat(checkHasFilms(restassuredController.getPeople(getRandomNumber(10))))
                 .overridingErrorMessage("This people does not have any films").isTrue();
     }
